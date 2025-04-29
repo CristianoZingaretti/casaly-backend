c5453c37486881a395a8cbc2e23690b25b8ed438
CristianoZingaretti <cristianozingaretti@hotmail.it>
2025-04-29 17:46:01 +0200

    Init commit setup maven

---
+++ casaly/src/main/java/com/casaly/casaly/CasalyApplication.java
@@ -0,0 +1,13 @@
+package com.casaly.casaly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasalyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasalyApplication.class, args);
+	}
+
+}
---
+++ casaly/src/test/java/com/casaly/casaly/CasalyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casaly.casaly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasalyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
